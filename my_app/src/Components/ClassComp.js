348f76d6a2d26f1a2c26abd112be38d5854ed65d
Sujata Athshere <sujataathshere@gmail.com>
2023-01-05 22:21:50 +0530

    Learn Functional Component & Class Component

---
+++ my_app/src/Components/ClassComp.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class ClassComp extends React.Component{
+    render(){
+        return <p>This is the Class Component</p>
+    }
+}
+export class ClassComp1 extends React.Component{
+    render(){
+        return <p>Hey ! I am another Class</p>
+    }
+}
---
+++ my_app/src/Components/FunctionalComp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FunctionalComp(){
+    return <p>This is a Functional Component</p>
+}
+
+export default FunctionalComp;
