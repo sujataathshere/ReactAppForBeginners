409e58c599f8a0deed02e037bca9743deb5e781f
Sujata Athshere <sujataathshere@gmail.com>
2023-01-14 22:40:39 +0530

    Impliment redux

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from "react";
+import NewComp from './NewComp';
+import { render } from '@testing-library/react';
+class App extends Component{
+  styles = {
+    fontStyle: 'bold',
+    color: 'teal'
+  }
+  render(){
+    return(
+      <div className='App'>
+        <h1 style={this.styles}>Welcome</h1>
+        <NewComp/>
+      </div>
+    )
+  }
 }
-
 export default App;
--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import reducer from './Store/Reducer';
+import { Provider } from 'react-redux';
 
+const store = createStore(reducer)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
